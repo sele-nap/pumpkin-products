786370529a659fa4ce467c3610305ef7a613caa8
Séléna <selena.poun@gmail.com>
2025-10-01 16:24:49 +0200

    delete all

--- server/src/config.js
+++
@@ -1,8 +0,0 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
-
-export const PORT = process.env.PORT || 4000;
-export const MONGO_URI = process.env.MONGO_URI;
-export const JWT_SECRET = process.env.JWT_SECRET;
-export const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || '7d';
